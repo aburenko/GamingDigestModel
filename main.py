84839167f76c805574208b7b2553c21467a10e98
Anton Burenko <anton.burenko@rwth-aachen.de>
2024-04-25 16:32:06 +0200

    Fix to first trainable state

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from torchvision.models import resnet18, ResNet18_Weights
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from PIL import Image
+from torchvision.transforms import transforms
 
 
 class HighlightModel(nn.Module):
@@ -20,7 +21,7 @@ class HighlightModel(nn.Module):
         self.lstm = nn.LSTM(input_size=256, hidden_size=256, num_layers=2, dropout=0.2, batch_first=True)
         self.fc1 = nn.Linear(256, 128)
         self.fc2 = nn.Linear(128, 2)
-        self.hidden = None
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         """The inputs are batched images of size (sequence size, channels, height, width)"""
@@ -28,12 +29,12 @@ class HighlightModel(nn.Module):
             x = self.resnet(x)
 
         x = self.fc_to_lstm(x)
-        x, self.hidden = self.lstm(x, self.hidden)
+        x, _ = self.lstm(x)
 
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
-        return x
+        return self.softmax(x)
 
 
 class StreamDataset(Dataset):
@@ -41,14 +42,21 @@ class StreamDataset(Dataset):
     def __init__(self, root_dir, phase):
         self.images = np.load(Path(root_dir) / phase / "images.npy")
         # paths from scalable are e.g.: http://localhost:8686/items/br-stream/br-stream/br-stream-0000012.jpg
-        self.images = [path.replace("http://localhost:8686/items/br-stream/", "~/data/") for path in self.images]
+        self.images = [path.replace("http://localhost:8686/items/", "/home/burenko/data/") for path in self.images]
         self.labels = np.load(Path(root_dir) / phase / "labels.npy")
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        image = Image.open(self.images[idx])
+        preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        image = preprocess(Image.open(self.images[idx]))
+
         label = self.labels[idx]
         return image, label
 
@@ -66,9 +74,10 @@ def train(model: HighlightModel, loader_train: DataLoader, loader_test: DataLoad
     n_epochs = 3
     for epoch in range(n_epochs):
         model.train()
+        torch.autograd.set_detect_anomaly(True)
         for i, (image_batch, label_batch) in enumerate(loader_train):
             prediction = model(image_batch)
-            loss = loss_fn(prediction, label_batch)
+            loss = loss_fn(prediction, F.one_hot(label_batch, num_classes=2).float())
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
